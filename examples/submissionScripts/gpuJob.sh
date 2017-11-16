cb7a5f28d6017ac11b3d9564c4b687a1fdb574df
ania.brown <ania.brown@gmail.com>
2017-11-16 14:55:32 +0000

    cleaned up example submission script

--- examples/submissionScripts/gpuJob.sh
+++ examples/submissionScripts/gpuJob.sh
@@ -4,16 +4,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 
-# Choose the right amount of memory to ask for
-##SBATCH --mem=50Gb
-
-# uncomment if NUM_QUBITS > 31
-####SBATCH --mem=100Gb
-# uncomment if NUM_QUBITS > 32
-####SBATCH --mem=200Gb
-
 # set max wallclock time
-#SBATCH --time=00:01:00
+#SBATCH --time=00:30:00
 
 # set name of job
 #SBATCH --job-name QUEST
@@ -22,7 +14,6 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpu
 ###SBATCH --partition=devel-gpu
-#SBATCH --constraint=k80
 
 NUM_QUBITS=27
 EXE=demo
@@ -31,9 +22,13 @@ export OMP_NUM_THREADS=16
 module purge
 module load gpu/cuda
 
-/system/software/arcus-b/gpu/cuda/7.0.28/samples/1_Utilities/deviceQuery/deviceQuery
+## uncomment for reporting about GPU used
+##/system/software/arcus-b/gpu/cuda/7.0.28/samples/1_Utilities/deviceQuery/deviceQuery
 
 make clean
 make
 
-nvprof ./$EXE $NUM_QUBITS
+./$EXE $NUM_QUBITS
+
+## uncomment to run a command line profiler
+##nvprof ./$EXE $NUM_QUBITS
